9b96508a6c5671df3540b8aade55c9c05cc3d4f4
K_melange <w3melessar@gmail.com>
2020-03-23 06:34:25 +0900

    욕구 초기치, 생성량 재설정 및 JSON 파일 작성하였음. 욕구 능력치 설정해서 캐릭터 생성하는 메서드 구현할 예정.

--- Assets/1.Scripts/Manager/GameManager.cs
+++ Assets/1.Scripts/Manager/GameManager.cs
@@ -407,8 +407,8 @@ public class GameManager : MonoBehaviour
             progressInformations[i].maxLevel = aData["scene"][sceneNumber]["progressInformation"]["maxLevel"].AsInt;
         }
 
-        traveler_Max = 1;
-        adventurer_Max = 0;
+        traveler_Max = 0;
+        adventurer_Max = 1;
         //specialAdventurer_Max = 800;
 
     }
