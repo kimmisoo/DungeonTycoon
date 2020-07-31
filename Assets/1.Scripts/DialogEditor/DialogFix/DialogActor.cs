da4d3e0cf376ebdcae06766d5cea76b1b1cb9a7d
cosmi710 <cosmi710@naver.com>
2020-07-31 19:56:36 +0900

    .

---
+++ Assets/1.Scripts/DialogEditor/DialogFix/DialogActor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//나레이션
+//
+public class DialogActor{
+
+	public string name;
+	public Sprite portrait;
+}
---
+++ Assets/1.Scripts/DialogEditor/DialogFix/DialogEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum DialogEffectName {Shake, FadeIn, FadeOut, DialogStart, DialogEnd, GrayScaleOn, GrayScaleOff, BackGroundChange, IllustrationChangeL, IllustrationChangeR}
+
+public class DialogEffect{
+
+	public virtual void Paly()
+	{
+
+	}
+
+	
+}
