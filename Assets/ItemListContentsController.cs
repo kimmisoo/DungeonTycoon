96b9757b0c85171544ea0a3d9fb543362190bebf
K_melange <w3melessar@gmail.com>
2020-11-25 19:33:27 +0900

    아이템 리스트 코드 구현 중.

---
+++ Assets/ItemListContentsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemListContentsController : MonoBehaviour
+{
+    private int rowNum, colNum;
+    //private float rowHeight, colWidth;
+    public GridLayoutGroup gridLayoutGroup;
+
+    public void OnEnable()
+    {
+        AdjustHeight();
+    }
+
+    public void GetGridAttribute()
+    {
+        //rowHeight = gridLayoutGroup.cellSize.y;
+        //colNum = gridLayoutGroup.
+        colNum = Mathf.FloorToInt(gameObject.GetComponent<RectTransform>().rect.width / gridLayoutGroup.cellSize.x);
+        rowNum = Mathf.CeilToInt(gameObject.transform.childCount / colNum); 
+    }
+
+    public void AdjustHeight()
+    {
+        GetGridAttribute();
+
+        float x, y, width, height;
+        x = gameObject.GetComponent<RectTransform>().rect.x;
+        y = gameObject.GetComponent<RectTransform>().rect.y;
+        width = gameObject.GetComponent<RectTransform>().rect.width;
+        height = rowNum * gridLayoutGroup.cellSize.y;
+        gameObject.GetComponent<RectTransform>().rect.Set(x, y, width, height);
+    }
+}
