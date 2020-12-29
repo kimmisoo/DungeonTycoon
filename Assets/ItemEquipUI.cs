a8f63a8efa23c90f025bdb5bd701413c88e34837
K_melange <w3melessar@gmail.com>
2020-12-30 08:38:33 +0900

    아이템 리스트 스크립트 구현 중

---
+++ Assets/ItemEquipUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEquipUI : MonoBehaviour
+{
+    private string selectedSlot = null;
+    public ItemListPanel listPanel;
+
+    public void SelectItemSlot(string inputSlot)
+    {
+        if (selectedSlot != inputSlot)
+        {
+            selectedSlot = inputSlot;
+
+            if (selectedSlot == "Accessory1" || selectedSlot == "Accessory2")
+            {
+                listPanel.ShowItemListByCategory("Accessory");
+            }
+            else
+                listPanel.ShowItemListByCategory(selectedSlot);
+        }
+    }
+}
---
+++ Assets/ItemListPanel.cs
@@ -0,0 +1,117 @@
+﻿using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemListPanel : MonoBehaviour
+{
+    private int rowNum, colNum;
+    //private float rowHeight, colWidth;
+    public GridLayoutGroup gridLayoutGroup;
+    public Dictionary<string, GameObject> itemList = new Dictionary<string, GameObject>();
+
+    void Start()
+    {
+        GameObject loadedPrefab = (GameObject)Resources.Load("UIPrefabs/TrainUI/ListContentsPanel");
+        itemList.Add("Weapon", Instantiate<GameObject>(loadedPrefab));
+        itemList.Add("Armor", Instantiate<GameObject>(loadedPrefab));
+        itemList.Add("Accessory", Instantiate<GameObject>(loadedPrefab));
+    }
+
+    void OnEnable()
+    {
+        AdjustHeight();
+    }
+
+    private void MakeItemListByCategory(string itemCategory)
+    {
+        GameObject loadedPrefab = (GameObject)Resources.Load("UIPrefabs/TrainUI/ListContentsPanel");
+        itemList.Add(itemCategory, Instantiate<GameObject>(loadedPrefab));
+
+    }
+
+    // 아이템 카테고리에 맞는 리스트 보이는 메서드
+    public void ShowItemListByCategory(string itemCategory)
+    {
+        //GetContents(itemList[selectedCategory], selectedCategory);
+        //AdjustHeight();
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            itemList.Values.ToList()[i].SetActive(false);
+        }
+        itemList[itemCategory].SetActive(true);
+    }
+
+    // 아이템별 아이템 프리팹 불러와서 패널에 집어넣는 메서드
+    private void GetContents(GameObject parent, string itemCategory)
+    {
+        JSONNode jsonNode = ItemManager.Instance.GetItemJSONNode();
+        //GameObject itemIcon = (GameObject)Resources.Load("UIPrefabs/TrainUI/ItemIcon_8");
+
+        for (int i = 0; i < jsonNode[itemCategory].Count; i++)
+        {
+            string iconPath = "UIPrefabs/TrainUI/ItemIcons/" + itemCategory + "/";
+            iconPath += jsonNode[itemCategory][i]["Name"];
+            Debug.Log(iconPath);
+            GameObject itemIcon = (GameObject)Resources.Load(iconPath);
+
+            GameObject newIcon = Instantiate<GameObject>(itemIcon);
+
+            GameObject iconImage = newIcon.transform.GetChild(1).gameObject;
+
+            newIcon.transform.SetParent(gameObject.transform);
+            newIcon.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            newIcon.transform.localPosition = new Vector3(0, 0, 0);
+        }
+        //JSONNode jsonNode = ItemManager.Instance.GetItemJSONNode();
+        ////GameObject itemIcon = (GameObject)Resources.Load("UIPrefabs/TrainUI/ItemIcon_8");
+
+        //for (int i = 0; i<jsonNode[itemCategory].Count; i++)
+        //{
+        //    string iconPath = "UIPrefabs/TrainUI/ItemIcons/" + itemCategory + "/";
+        //    iconPath += jsonNode[itemCategory][i]["Name"];
+        //    Debug.Log(iconPath);
+        //    GameObject itemIcon = (GameObject)Resources.Load(iconPath);
+
+        //    GameObject newIcon = Instantiate<GameObject>(itemIcon);
+
+        //    GameObject iconImage = newIcon.transform.GetChild(1).gameObject;
+
+        //    newIcon.transform.SetParent(gameObject.transform);
+        //    newIcon.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        //    newIcon.transform.localPosition = new Vector3(0, 0, 0);
+        //}
+    }
+
+    // 그리드 레이아웃에서 필요한 정보 가져오는 메서드
+    private void GetGridLayoutAttribute()
+    {
+        //rowHeight = gridLayoutGroup.cellSize.y;
+        //colNum = gridLayoutGroup.
+        colNum = Mathf.FloorToInt(gameObject.GetComponent<RectTransform>().rect.width / gridLayoutGroup.cellSize.x);
+        rowNum = Mathf.CeilToInt((float)gameObject.transform.childCount / colNum); 
+    }
+
+    // 내용물 양에 따라 크기 조절
+    private void AdjustHeight()
+    {
+        GetGridLayoutAttribute();
+
+        float x, y, width, height;
+        x = gameObject.GetComponent<RectTransform>().rect.x;
+        y = gameObject.GetComponent<RectTransform>().rect.y;
+        width = gameObject.GetComponent<RectTransform>().rect.width;
+        height = rowNum * gridLayoutGroup.cellSize.y;
+        //gameObject.GetComponent<RectTransform>().rect.Set(x, y, width, height);
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        //Debug.Log("AdjustingHeight " + height + "\n" + "new: " + gameObject.GetComponent<RectTransform>().rect.height);
+    }
+
+    //private void RefreshGridLayout()
+    //{
+    //    LayoutRebuilder.ForceRebuildLayoutImmediate(gameObject.GetComponent<RectTransform>());
+    //    //gameObject.GetComponent<GridLayoutGroup>()
+    //}
+}
