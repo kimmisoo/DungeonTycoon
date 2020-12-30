7431fcf0c86b79756ee91fe0c243d925c4e1ce3e
K_melange <w3melessar@gmail.com>
2020-12-30 19:21:52 +0900

    리스트 패널 생성 구현 중

--- Assets/ItemListPanel.cs
+++ Assets/ItemListPanel.cs
@@ -7,29 +7,34 @@ using UnityEngine.UI;
 
 public class ItemListPanel : MonoBehaviour
 {
-    private int rowNum, colNum;
+    //private int rowNum, colNum;
     //private float rowHeight, colWidth;
-    public GridLayoutGroup gridLayoutGroup;
     public Dictionary<string, GameObject> itemList = new Dictionary<string, GameObject>();
 
     void Start()
     {
         GameObject loadedPrefab = (GameObject)Resources.Load("UIPrefabs/TrainUI/ListContentsPanel");
-        itemList.Add("Weapon", Instantiate<GameObject>(loadedPrefab));
-        itemList.Add("Armor", Instantiate<GameObject>(loadedPrefab));
-        itemList.Add("Accessory", Instantiate<GameObject>(loadedPrefab));
+
+        MakeItemListByCategory("Weapon", loadedPrefab);
+        MakeItemListByCategory("Armor", loadedPrefab);
+        MakeItemListByCategory("Accessory", loadedPrefab);
     }
 
     void OnEnable()
     {
-        AdjustHeight();
+        
     }
 
-    private void MakeItemListByCategory(string itemCategory)
+    private void MakeItemListByCategory(string itemCategory, GameObject loadedPrefab)
     {
-        GameObject loadedPrefab = (GameObject)Resources.Load("UIPrefabs/TrainUI/ListContentsPanel");
-        itemList.Add(itemCategory, Instantiate<GameObject>(loadedPrefab));
+        string prefabName = "ListContentsPanel ";
 
+        GameObject newObject = Instantiate<GameObject>(loadedPrefab);
+        newObject.transform.SetParent(gameObject.transform);
+        newObject.name = prefabName + itemCategory;
+        AdjustHeight(newObject);
+        itemList.Add(itemCategory, newObject);
+        LoadContents(newObject, itemCategory);
     }
 
     // 아이템 카테고리에 맞는 리스트 보이는 메서드
@@ -45,7 +50,7 @@ public class ItemListPanel : MonoBehaviour
     }
 
     // 아이템별 아이템 프리팹 불러와서 패널에 집어넣는 메서드
-    private void GetContents(GameObject parent, string itemCategory)
+    private void LoadContents(GameObject parent, string itemCategory)
     {
         JSONNode jsonNode = ItemManager.Instance.GetItemJSONNode();
         //GameObject itemIcon = (GameObject)Resources.Load("UIPrefabs/TrainUI/ItemIcon_8");
@@ -61,7 +66,7 @@ public class ItemListPanel : MonoBehaviour
 
             GameObject iconImage = newIcon.transform.GetChild(1).gameObject;
 
-            newIcon.transform.SetParent(gameObject.transform);
+            newIcon.transform.SetParent(itemList[itemCategory].transform);
             newIcon.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             newIcon.transform.localPosition = new Vector3(0, 0, 0);
         }
@@ -86,26 +91,29 @@ public class ItemListPanel : MonoBehaviour
     }
 
     // 그리드 레이아웃에서 필요한 정보 가져오는 메서드
-    private void GetGridLayoutAttribute()
-    {
-        //rowHeight = gridLayoutGroup.cellSize.y;
-        //colNum = gridLayoutGroup.
-        colNum = Mathf.FloorToInt(gameObject.GetComponent<RectTransform>().rect.width / gridLayoutGroup.cellSize.x);
-        rowNum = Mathf.CeilToInt((float)gameObject.transform.childCount / colNum); 
-    }
+    //private void GetGridLayoutAttribute()
+    //{
+    //    //rowHeight = gridLayoutGroup.cellSize.y;
+    //    //colNum = gridLayoutGroup.
+    //    colNum = Mathf.FloorToInt(gameObject.GetComponent<RectTransform>().rect.width / gridLayoutGroup.cellSize.x);
+    //    rowNum = Mathf.CeilToInt((float)gameObject.transform.childCount / colNum);
+    //}
 
     // 내용물 양에 따라 크기 조절
-    private void AdjustHeight()
+    private void AdjustHeight(GameObject listObject)
     {
-        GetGridLayoutAttribute();
+        GridLayoutGroup gridLayoutGroup = listObject.GetComponent<GridLayoutGroup>();
+        //GetGridLayoutAttribute();
+        int colNum = Mathf.FloorToInt(listObject.GetComponent<RectTransform>().rect.width / gridLayoutGroup.cellSize.x);
+        int rowNum = Mathf.CeilToInt((float)listObject.transform.childCount / colNum);
 
         float x, y, width, height;
-        x = gameObject.GetComponent<RectTransform>().rect.x;
-        y = gameObject.GetComponent<RectTransform>().rect.y;
-        width = gameObject.GetComponent<RectTransform>().rect.width;
+        x = listObject.GetComponent<RectTransform>().rect.x;
+        y = listObject.GetComponent<RectTransform>().rect.y;
+        width = listObject.GetComponent<RectTransform>().rect.width;
         height = rowNum * gridLayoutGroup.cellSize.y;
         //gameObject.GetComponent<RectTransform>().rect.Set(x, y, width, height);
-        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        listObject.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
         //Debug.Log("AdjustingHeight " + height + "\n" + "new: " + gameObject.GetComponent<RectTransform>().rect.height);
     }
 
