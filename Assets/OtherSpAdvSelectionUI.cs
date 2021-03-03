eb7cdab20c95cfc8716adadef9407dd5f9aae644
K_melange <w3melessar@gmail.com>
2021-03-03 18:09:53 +0900

    다른 모험가 정보 보기 기능 구현 중

---
+++ Assets/OtherSpAdvSelectionUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherSpAdvSelectionUI : MonoBehaviour
+{
+    public GameObject spAdvList;
+    private Dictionary<string, OtherSpAdvSelectButton> buttons;
+
+    private void Awake()
+    {
+        buttons = new Dictionary<string, OtherSpAdvSelectButton>();
+
+        for (int i = 0; i < spAdvList.transform.childCount; i++)
+        {
+            OtherSpAdvSelectButton curBtn = spAdvList.transform.GetChild(i).GetComponent<OtherSpAdvSelectButton>();
+            buttons.Add(curBtn.nameKey, curBtn);
+        }
+        buttons[GameManager.Instance.GetPlayerSpAdv().GetComponent<SpecialAdventurer>().nameKey].HighlightOn();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+}
